38e3e0fe788cb9f40f12901964ba7caec258bf5d
Evgeny Kolodin <kolodinevgenij@MacBook-Pro-Kolodin.local>
2022-08-01 12:14:29 +0300

    some crawler tests and fixes

--- anadet/crawler.py
+++ anadet/crawler.py
@@ -1,22 +1,22 @@
 from pathlib import Path
 
 class Crawler:
-    IGNORE_DIRECTORIES = ["__pycache__", ".DS_Store"]
+    IGNORE_ITEMS = ["__pycache__", ".DS_Store"]
     def __init__(self, root):
         self.root_dir = Path(root)
 
     def getFiles(self):
         return self._getFiles(self.root_dir)
 
-    def _getFiles(self, dir):
+    def _getFiles(self, dir: Path):
         filelist = []
-
         for item in dir.iterdir():
+            if item.name in self.IGNORE_ITEMS:
+                continue
             if item.is_file():
                 filelist.append(str(item.resolve()))
             else:
-                if str(item) not in self.IGNORE_DIRECTORIES:
-                    filelist.extend(self.getFiles(item))
+                filelist.extend(self._getFiles(item))
         return filelist
 
 
--- tests/test_crawler.py
+++ tests/test_crawler.py
@@ -4,8 +4,16 @@ from anadet.crawler import Crawler
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-def test_file_detector_class_exists():
+def test_file_crawler_class_exists():
     assert BASE_DIR.joinpath("anadet/crawler.py").is_file()
 
-
-    
+def test_crawl_test_dir():
+    cr = Crawler(BASE_DIR.joinpath('tests/test_resources'))
+    files = cr.getFiles()
+    assert len(files) == 4
+    names = [i.split('/')[-1] for i in files]
+    assert 'results-master_h1_DiagDetPV-3-Spec.csv' in names
+    assert 'results-master_h1_XWallDetPV-6-Theta.csv' in names
+    assert 'results-master_h1_DiagDetPV-2-Phi.csv' in names
+    assert 'results-master_h1_VertDetPV-8-Phi.csv' in names
+    assert 'not_tracked_file.csv' not in names
